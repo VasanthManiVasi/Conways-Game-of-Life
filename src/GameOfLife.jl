c3cc37c2d809a259b5a972bad32930ced525078b
vasanth <vasanth.mani.vasi.2@gmail.com>
2021-03-02 19:51:46 +0530

    Add title in plots and a speed parameter for the simulation

--- src/GameOfLife.jl
+++ src/GameOfLife.jl
@@ -6,10 +6,17 @@ include("World.jl")
 using Plots, Images
 gr()
 
-function simulate(world = WorldState())
-    for days in world
-        display(plot(Gray.(world.current_state)))
-        sleep(0.25)
+function simulate(;max_days::Int = 100, initial_state::Array = GameOfLife.default_state(), speed::Int = 90)
+    if !(0 < speed < 101)
+        throw("speed must be between 1 to 100")
+    end
+
+    world = WorldState(max_days, initial_state)
+    for (days, generation) in zip(world, 1:max_days)
+        p = plot(Gray.(world.current_state .== 0), framestyle = :none)
+        title!(string("Generation ", generation))
+        display(p)
+        sleep(1 - (speed * 0.01))
     end
 end
 
